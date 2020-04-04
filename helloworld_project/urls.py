27742f12e6272901f2fdc5e6ecbee7ed02da4deb
vg-devops <vkr.special@gmail.com>
2020-04-04 20:30:13 +0100

    comment added

--- helloworld_project/urls.py
+++ helloworld_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+## just comment
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
